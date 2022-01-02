6d47d710080fa6d5e6b7ff725672967d6680f249
Tazzmanian <tazzmanian@abv.bg>
2022-01-03 00:02:48 +0200

    Java 9 features

--- hello-world/src/App.java
+++ hello-world/src/App.java
@@ -1,5 +1,17 @@
+import java.lang.ProcessHandle;
+
 public class App {
+
+    // Java 9
+    private static void processHandleMethod() {
+        ProcessHandle currProcess = ProcessHandle.current();
+        ProcessHandle.Info info = currProcess.info();
+        System.out.println("PID: " + currProcess.pid());
+        System.out.println("Info: " + info);
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        processHandleMethod();
     }
 }
